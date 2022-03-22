5a11787b1bf8530b79599c112643ba7f9f989b33
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-22 11:09:18 +0800

    edited cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://boulder-bike-tour-backend.herokuapp.com:80'
-    # origins '*'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins 'http://localhost:3001'
 
     resource '*',
       headers: :any,
