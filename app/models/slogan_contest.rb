f01248555a6dda36c67f710b1599585337710e08
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-03-22 09:16:31 +0800

    Edited validation condition for sloganContest model

--- app/models/slogan_contest.rb
+++ app/models/slogan_contest.rb
@@ -2,7 +2,7 @@ class SloganContest < ApplicationRecord
   # Validations
   validates :first_name, presence: true, length: { in: 1..200 }
   validates :last_name, presence: true, length: { in: 1..200 }
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
   validates :slogan, presence: true, length: { in: 1..50 }
 
 end
